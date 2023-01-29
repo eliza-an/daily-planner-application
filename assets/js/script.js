fc35734ff65168bbbaa98d675a549a8fc246d10a
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-01-29 19:19:36 +0000

    added a basic outline. Created an exact date and time display to the second. Started working on the display of the 9-5 timetable by adding the UL tags and formatting with css

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//
+function UpdatedTime(){
+ var HeadingDate= moment().format('MMMM Do YYYY, h:mm:ss a')
+ $('#currentDay').html( "Current Date and Time: "+ HeadingDate )
+}
+
+
+//Making sure the time updates every second and gives me a new value
+UpdatedTime();
+setInterval(function(){
+   UpdatedTime();
+},1000);
+
+
