2f3f4390ae84714f94613704525d47d68a150c9c
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-01-31 23:34:57 +0000

    fixed the issue of the nulls showing up and now I am going to bed

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,7 @@ setInterval(function(){
 var now=moment()
 var time=now.hour()
 
+console.log(now)
 
 arr.forEach((value, index) => {
    if (time>index+9) {
@@ -65,77 +66,147 @@ var arr = Array.from( lis )
 
 //the save functionality for each individual button
 
-
+//1
 function input1() {
-if(form1.value!==null){
+
    localStorage.setItem("9am", form1.value);
+ 
 }
+let original1= localStorage.getItem("9am")
+
+
+if (original1 ===null){
+   document.getElementById("form1").setAttribute("value","")
+}else{
+   document.getElementById("form1").setAttribute("value",localStorage.getItem("9am")) 
 }
-document.getElementById("form1").setAttribute("value",localStorage.getItem("9am"))
 saveBtn1.addEventListener("click",input1)
 
+
+//2
 function input2() {
-   if(form2.value!==null){
    localStorage.setItem("10am", form2.value);
    
-}}
-document.getElementById("form2").setAttribute("value",localStorage.getItem("10am"))
+}
+
+
+let original2= localStorage.getItem("10am")
+if (original2 ===null){
+   document.getElementById("form2").setAttribute("value","")
+}else{
+   document.getElementById("form2").setAttribute("value",localStorage.getItem("10am")) 
+}
+
+
 saveBtn2.addEventListener("click",input2)
+
+//3
 
 
 function input3() {
-   if(form3.value!==null){
    localStorage.setItem("11am", form3.value);
    
-}}
-   document.getElementById("form3").setAttribute("value",localStorage.getItem("11am"))
+}
+
+let original3= localStorage.getItem("11am")
+if (original3 ===null){
+   document.getElementById("form3").setAttribute("value","")
+}else{
+   document.getElementById("form3").setAttribute("value",localStorage.getItem("11am")) 
+}
 saveBtn3.addEventListener("click",input3)
 
 
+
+//4 
+
 function input4() {
-   if(form4.value!==null){
    localStorage.setItem("12pm", form4.value);
-}}
-   document.getElementById("form4").setAttribute("value",localStorage.getItem("12pm"))
+}
+
+let original4= localStorage.getItem("12pm")
+if (original4 ===null){
+   document.getElementById("form4").setAttribute("value","")
+}else{
+   document.getElementById("form4").setAttribute("value",localStorage.getItem("12pm")) 
+}
 saveBtn4.addEventListener("click",input4)
 
 
+
+//5
+
 function input5() {
-   if(form5.value!==null){
    localStorage.setItem("1pm", form5.value);
-   }}
-   document.getElementById("form5").setAttribute("value",localStorage.getItem("1pm"))
+   }
+  
+   let original5= localStorage.getItem("1pm")
+if (original5 ===null){
+   document.getElementById("form5").setAttribute("value","")
+}else{
+   document.getElementById("form5").setAttribute("value",localStorage.getItem("1pm")) 
+}
 saveBtn5.addEventListener("click",input5)
 
 
+
+//6
+
 function input6() {
-   if(form6.value!==null){
    localStorage.setItem("2pm", form6.value);
-   }}
-   document.getElementById("form6").setAttribute("value",localStorage.getItem("2pm"))
+   }
+
+   let original6= localStorage.getItem("2pm")
+   if (original6 ===null){
+      document.getElementById("form6").setAttribute("value","")
+   }else{
+      document.getElementById("form6").setAttribute("value",localStorage.getItem("2pm")) 
+   }
 saveBtn6.addEventListener("click",input6)
 
 
+
+//7
 function input7() {
-   if(form7.value!==null){
    localStorage.setItem("3pm", form7.value);
- }}
-   document.getElementById("form7").setAttribute("value",localStorage.getItem("3pm"))
+ }
+
+ let original7= localStorage.getItem("3pm")
+ if (original7 ===null){
+    document.getElementById("form7").setAttribute("value","")
+ }else{
+    document.getElementById("form7").setAttribute("value",localStorage.getItem("3pm")) 
+ }
 saveBtn7.addEventListener("click",input7)
 
 
+
+//8
 function input8() {
-   if(form8.value!==null){
    localStorage.setItem("4pm", form8.value);
- }}
-   document.getElementById("form8").setAttribute("value",localStorage.getItem("4pm"))
+ }
+  
+ let original8= localStorage.getItem("4pm")
+if (original8 ===null){
+   document.getElementById("form8").setAttribute("value","")
+}else{
+   document.getElementById("form8").setAttribute("value",localStorage.getItem("4pm")) 
+}
 saveBtn8.addEventListener("click",input8)
 
 
+//9
+
+
 function input9() {
-   if(form9.value!==null){
    localStorage.setItem("5pm", form9.value);
-}}
-   document.getElementById("form9").setAttribute("value",localStorage.getItem("5pm"))
+}
+  
+let original9= localStorage.getItem("5pm")
+if (original9 ===null){
+   document.getElementById("form9").setAttribute("value","")
+}else{
+   document.getElementById("form9").setAttribute("value",localStorage.getItem("5pm")) 
+}
 saveBtn9.addEventListener("click",input9)
 
